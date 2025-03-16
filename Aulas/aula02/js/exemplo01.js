408796dff6fc64f32cbb6075d1ddd6b1486c3726
Cris <cristian.marcheti@etec.sp.gov.br>
2025-03-16 12:16:02 -0300

    aula02

--- Aulas/aula02/js/exemplo01.js
+++ Aulas/aula02/js/exemplo01.js
@@ -2,41 +2,41 @@ $("#esconder").click(function(){
     $("#teste1").hide();
 });
 
-$("#mostrar").click(function(){
+$("#mostrar").click(function (){
     $("#teste1").show();
 });
 
-$("#alterarCSS").click(function(){
+$("#alterarCSS").click(function (){
     $("#teste2").css({
-        "background-color" : "blue" ,
-        "font-size" : "200%" ,
-        "color" : "white" 
+        "background-color": "blue",
+        "font-size": "200%",
+        "color": "white"
     });
 });
 
-$("#alterarClass").click(function(){
+$("#alterarClass").click(function (){
     $("#teste3").removeClass("alert-success").addClass("alert-primary");
     $("#alterarClass").removeClass("btn-success").addClass("btn-danger");
 });
 
-$("#alternar1").hide();
+$("#alternar1").hide()
 
 $("#alternar1").click(function(){
     $("#link").attr({
-        "href" : "https://www.cps.sp.gov.br",
-        "title" :"CPS"
-    })
+        "href": "https://etecitu.cps.sp.gov.br/",
+        "title": "CPS"
+    });
     $("#link").text("CPS");
-    $("#alternar1").hide();
-    $("#alternar2").show();
+    $("#alternar1").hide()
+    $("#alternar2").show()
 });
 
 $("#alternar2").click(function(){
     $("#link").attr({
-        "href" : "https://etecitu.cps.sp.gov.br/",
-        "title" :"Etec Itu"
+        "href": "https://etecitu.cps.sp.gov.br/",
+        "title": "Etec Itu"
     });
     $("#link").text("Etec Itu");
-    $("#alternar2").hide();
-    $("#alternar1").show();
+    $("#alternar2").hide()
+    $("#alternar1").show()
 });
